c951f5a65ad2cf8c42e819cc364e61bd6d9eb460
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 20:28:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function theBeatlesPlay(musician, instruments) {
    }
    return arr
 }
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = [];
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return shoutedFacts
+}
