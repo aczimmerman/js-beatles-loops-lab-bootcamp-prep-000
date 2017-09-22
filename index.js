945a892787c82ca302a22f30a2691f3da39df14f
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 20:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician, instruments) {
   var arr = [];
-   for (let i = 0; i < arr.length; i++) {
-     instruments.push("John Lennon plays guitar")
+   for (let i = 0, l = musicians.length; i < l, i++) {
+     arr.push(`${musicians[i]} plays ${instruments[i]}`)
    }
    return arr
 }
