c3e0d61fd39b1d6d13e1f9a20f86ed27621e1e80
Andrea Zimmerman <andrea.c.zimmerman@gmail.com>
2017-09-22 20:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   const arr = [];
   do {
-    arr.push("I love the Beatles")
+    arr.push("I love the Beatles!")
     n++
   } while (n < 15)
   return arr
